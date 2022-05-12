8a3970ba6f93c94bdbfe30916aa2a9ccd2a16176
canosa92 <101671780+canosa92@users.noreply.github.com>
2022-05-12 18:20:45 +0200

    Add files via upload

---
+++ javascript/2/ejercicio 2.js
@@ -0,0 +1,11 @@
+var lista=["Adrian",29,false]
+console.log(lista)
+var Objeto= {
+    titulo:"Harry Potter",
+    autor:"J.K.Rowling",
+} 
+var fecha=new Date("12 18 1992")
+console.log(fecha)
+
+var lista3=[lista,fecha,Objeto]
+console.log(lista3)
