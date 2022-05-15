92b9b5f2b61d0415d8cd0aa6c97f26a23b03a95f
canosa92 <101671780+canosa92@users.noreply.github.com>
2022-05-15 17:14:51 +0200

    Add files via upload

---
+++ javascript/4/ejercicio 4.js
@@ -0,0 +1,22 @@
+var nombre="Adrian"
+var apellido="Canosa"
+var estudiante= nombre +" "+ apellido
+console.log(estudiante)
+var estudianteMayus= estudiante.toUpperCase()
+console.log(estudianteMayus)
+var estudianteminus=estudiante.toLowerCase()
+console.log(estudianteminus)
+var longitud=estudiante.length
+console.log(longitud)
+var n=nombre.charAt(0)
+console.log(n)
+console.log(apellido.length)
+var a=apellido[5]
+console.log(a)
+
+var espacio=estudiante.replace(/ /g,"")
+console.log(espacio)
+
+var boolenao=estudiante.includes(nombre)
+console.log(boolenao)
+
