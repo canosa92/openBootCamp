d94be324cabb3d965968c306eced4aca21f0b41b
canosa92 <101671780+canosa92@users.noreply.github.com>
2022-05-15 18:57:10 +0200

    Add files via upload

---
+++ javascript/5/5.js
@@ -0,0 +1,10 @@
+let altura= 187;
+console.log(altura);
+let altura2=1.87;
+console.log(altura2);
+let peso=107.60;
+let alturaredonda=altura2.toFixed(1)
+console.log(alturaredonda)
+let kgredondo=Math.round(peso)
+console.log(kgredondo)
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
