f44c1744c0b737b62b574aa20c3097a167c0ae2d
canosa92 <101671780+canosa92@users.noreply.github.com>
2022-05-24 13:49:17 +0200

    Add files via upload

---
+++ javascript/14/index.js
@@ -0,0 +1,10 @@
+const boton=document.querySelector("#boton")
+
+boton.addEventListener("click", ()=>
+alert("click en el botton")
+&&
+)
+
+$("button").click(function() {
+    console.log("Hola, estoy utilizando jQuery")
+})
