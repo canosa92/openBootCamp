ff2d0906efd456f87345c159803d99f2a705e7ac
canosa92 <101671780+canosa92@users.noreply.github.com>
2022-05-26 23:48:58 +0200

    Add files via upload

---
+++ python/ejercicio 2/2.2.py
@@ -0,0 +1,21 @@
+>>> hola="Hola que tal estas"
+>>> print(hola)
+Hola que tal estas
+>>> hola.type
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: 'str' object has no attribute 'type'
+>>> print(type(hola)
+... print(type(hola))
+  File "<stdin>", line 1
+    print(type(hola)
+          ^^^^^^^^^
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print(type(hola))
+<class 'str'>
+>>> hola.split()
+['Hola', 'que', 'tal', 'estas']
+>>> hola="Muy bien, gracias"
+>>> print(hola)
+Muy bien, gracias
+>>>
